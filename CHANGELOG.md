61888bd0b865ce08a7a8a34aa886ec76c0c1d586
Mon Sucher <msucher@umn.edu>
2021-04-27 12:17:47 -0500

    Reoganized setup scripts

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+## v0.1.5
+
+Adding support for ReCaptcha v2 with a simple settings. V3 is still kind of janky to use in some places.
+
+## v0.1.4
+
+Replaced  ReCaptcha V2 form widgets with V3 and removed `wagtail-recaptcha` since that project is dead. 
+
+## v0.1 - Mar 11, 2021
+
+Initial proof of concept
--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ from setuptools import find_packages, setup
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 
-with open('README.md') as readme:
-    README = readme.read()
+long_desc = open('README.md', 'rb').read().decode('utf-8')
 
 
 def get_requirements_tests():
@@ -17,15 +16,38 @@ def get_requirements_tests():
 
 setup(
     name='wagtailhoneypot',
+    version='1.0.2',
+    description='A wagtail package built on top of Django-captcha to add some honeypot fields for wagtail form builder, honeypot widgets can also be used with native Django forms.',
+    long_desciption=long_desc,
+    author='Mon Sucher',
+    author_email='supawaza@gmail.com',
+    url='https://github.com/suchermon/wagtailhoneypot.git',
     include_package_data=True,
     install_requires=[
         'flashtext',
+        'django>3,<4',
         'django-recaptcha',
         'wagtail>=2.12',
     ],
-    packages=find_packages('.'),
+    license='MIT',
+    keywords=['django', 'reCAPTCHA', 'honeypot', 'forms', 'anti-spam', 'reCAPCHA v2'],
+    packages=find_packages(),
     package_data={'wagtailhoneypot': [
         "templates/wagtailhoneypot/*.html",
         "templates/wagtailhoneypot/*/*.html",
     ]},
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0.3',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ]
 )
