56ab339b0e6190ac3d36f671c47deae024814ea0
Mon Sucher <suche010@umn.edu>
2021-06-17 11:17:19 -0500

    Fixed check for input

--- wagtailhoneypot/static/hp_form.js
+++ wagtailhoneypot/static/hp_form.js
@@ -1,6 +1,6 @@
 let submitBtn = document.querySelector('[type="submit"]');
 
-if(submitBtn.length) {
+if(submitBtn !== null) {
   submitBtn.addEventListener('click', function(e) {
     document.querySelectorAll('[data-js="hp-formfield"]').forEach(el => el.removeAttribute('required'));
   });
